00e73b9aa3456e63e3475b92183cc81f4a2f042b
ivanavocode <ivanavocode@gmail.com>
2020-06-26 17:42:07 +0600

    upd

--- js/main.js
+++ js/main.js
@@ -3,5 +3,13 @@ $(function(){
         nextArrow: '<button class="slick-arrow slick-next"><img src="images/next.svg" alt="next arrow"></button>',
         prevArrow: '<button class="slick-arrow slick-prev"><img src="images/prev.svg" alt="prev arrow"></button>',
         fade: true, 
+        responsive: [
+            {
+                breakpoint: 441,
+                settings: {
+                  arrows: false
+                }
+            }
+        ]
     });
 });
